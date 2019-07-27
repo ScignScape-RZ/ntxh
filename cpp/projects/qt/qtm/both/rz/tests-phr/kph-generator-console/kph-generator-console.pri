51231ee0d5f0f57afb1f6a77d82c290e6de86c49
scignscape-rz <osrworkshops@gmail.com>
2019-07-27 17:17:14 -0400

    update

---
+++ cpp/projects/qt/qtm/both/rz/tests-phr/kph-generator-console/kph-generator-console.pri
@@ -0,0 +1,81 @@
+
+#           Copyright Nathaniel Christen 2019.
+#  Distributed under the Boost Software License, Version 1.0.
+#     (See accompanying file LICENSE_1_0.txt or copy at
+#           http://www.boost.org/LICENSE_1_0.txt)
+
+PROJECT_NAME = kph-generator-console
+
+include(../build-group.pri)
+
+TEMPLATE = app
+
+INCLUDEPATH += $$SRC_DIR $$SRC_GROUP_DIR
+
+INCLUDEPATH += $$PHAONIR_SRC_GROUP_DIR
+INCLUDEPATH += $$PHAONLIB_SRC_GROUP_DIR
+
+INCLUDEPATH += $$PHR_RUNTIME_SRC_GROUP_DIR
+
+#INCLUDEPATH += $$PHAON_SRC_GROUP_DIR
+#INCLUDEPATH += $$HGDM_SRC_GROUP_DIR
+#INCLUDEPATH += $$KAUVIR_PHAON_SRC_GROUP_DIR
+#INCLUDEPATH += $$KAUVIR_KCM_SRC_GROUP_DIR
+#INCLUDEPATH += $$KCM_RUNTIME_EVAL_SRC_GROUP_DIR
+#INCLUDEPATH += $$KAUVIR_RUNTIME_SRC_GROUP_DIR
+
+#
+
+INCLUDEPATH += $$RELAE_GRAPH_SRC_GROUP_DIR
+
+DEFINES += DEFAULT_PTR_BYTE_CODE=QT_POINTER_SIZE
+
+
+CONFIG += no_keywords
+
+
+DEFINES += USE_KANS
+
+DEFINES += DEFAULT_KPH_FOLDER=\\\"$$CPP_ROOT_DIR/kph\\\"
+
+HEADERS += \
+  $$SRC_DIR/test-functions.h \
+
+
+SOURCES += \
+  $$SRC_DIR/main.cpp \
+  $$SRC_DIR/test-functions.cpp \
+
+LIBS += -L$$TARGETSDIR -lkph-generator -lphaon-ir \
+  -lphr-direct-eval  -lphr-env -lphaon-lib -lphr-fn-doc \
+  -lphr-command-runtime
+
+#LIBS += -L$$TARGETSDIR -lPhaonLib -lkauvir-code-model -lkauvir-type-system \
+#   -lkcm-command-package -lkcm-direct-eval -lkcm-scopes -lkauvir-phaon -lkcm-command-runtime
+
+
+contains(CHOICE_FEATURES, "kcm_ecl") \#/
+{
+ LIBS += -L$$TARGETSDIR -lkcm-lisp-bridge -lrz-dynamo-generator
+ include(../../../../find-ecl-sexp.pri)
+ LIBS += -L$$ECL_DIR -lecl
+ LIBS += -L$$CL_CXX_DIR/install/lib64 -lcl_cxx
+}
+
+
+contains(CHOICE_FEATURES, "iso-choice") \#/
+{
+ exists($$CPP_ROOT_DIR/targets/$$CHOICE_CODE/kauvir--kauvir-kcm--kcm-lisp-bridge) \#/
+ {
+  LIBS += -L$$TARGETSDIR -lkcm-lisp-bridge -lrz-dynamo-generator
+  message(DEFINE\'ing ISO__USING_ECL)
+  DEFINES += ISO__USING_ECL
+  include(../../../../find-ecl-sexp.pri)
+  LIBS += -L$$ECL_DIR -lecl
+  LIBS += -L$$CL_CXX_DIR/install/lib64 -lcl_cxx
+ }
+}
+
+
+message(choice: $$CPP_ROOT_DIR/targets/$$CHOICE_CODE/$$PROJECT_SET--$$PROJECT_GROUP--$$PROJECT_NAME)
+mkpath($$CPP_ROOT_DIR/targets/$$CHOICE_CODE/$$PROJECT_SET--$$PROJECT_GROUP--$$PROJECT_NAME)
---
+++ cpp/src/rz/tests-phr/kph-generator-console/main.cpp
@@ -0,0 +1,150 @@
+
+//           Copyright Nathaniel Christen 2019.
+//  Distributed under the Boost Software License, Version 1.0.
+//     (See accompanying file LICENSE_1_0.txt or copy at
+//           http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include "kph-generator/kph-generator.h"
+
+#include "phaon-ir/channel/phr-channel-group.h"
+
+#include "phaon-ir/phr-code-model.h"
+
+#include "phr-direct-eval/phr-direct-eval.h"
+
+#include "phaon-ir/table/phr-symbol-scope.h"
+
+#include "phaon-ir/runtime/phr-command-package.h"
+
+#include "phaon-lib/phr-runner.h"
+
+//#include "phaon-ir/
+
+//#include "kauvir-phaon/kph-command-package.h"
+//#include "kauvir-code-model/kauvir-code-model.h"
+//#include "kauvir-code-model/kcm-channel-group.h"
+//#include "kcm-scopes/kcm-scope-system.h"
+
+
+//#include "PhaonLib/phaon-namespace.h"
+//#include "PhaonLib/phaon-class.h"
+//#include "PhaonLib/phaon-function.h"
+//#include "PhaonLib/phaon-symbol-scope.h"
+
+#include "relae-graph/relae-caon-ptr.h"
+
+#include "phr-env/phr-env.h"
+#include "phr-fn-doc/phr-fn-doc.h"
+
+#include "test-functions.h"
+
+//#include "PhaonLib/phaon-channel-group-table.h"
+//#include "PhaonLib/phaon-runner.h"
+//#include "kcm-direct-eval/kcm-direct-eval.h"
+
+#include <QObject>
+
+#include "kans.h"
+
+
+//?USING_KANS(Phaon)
+
+
+#include "phaon-ir/phaon-ir.h"
+#include "phaon-ir/channel/phr-channel-system.h"
+
+
+int main(int argc, char* argv[])
+{
+// PHR_Runner phr;
+//// Phaon_Namespace phn("TestNS");
+//// Phaon_Class phc("Test_Class", &phn);
+// PHR_Code_Model& pcm = phr.get_pcm();
+// pcm.set_direct_eval_fn(&phr_direct_eval);
+
+ PHR_Channel_System pcs;
+
+ PhaonIR phr(&pcs);
+// PHR_Runner phrn;
+// phrn.
+
+ phr.init_type_system();
+ phr.init_type("fbase", DEFAULT_PTR_BYTE_CODE);
+ phr.init_type("u4", 4);
+ phr.init_type("u8", 8);
+ phr.init_type("argvec", 9);
+ phr.init_type("pcv", DEFAULT_PTR_BYTE_CODE);
+ phr.init_type("str", DEFAULT_PTR_BYTE_CODE);
+
+ qRegisterMetaType<PHR_Fn_Doc>();
+ qRegisterMetaType<PHR_Fn_Doc*>();
+
+ // //  setup
+ phr.init_code_model();
+
+ PHR_Code_Model& pcm = *phr.code_model();
+ pcm.set_type_system(phr.type_system());
+
+ phr.create_channel_semantic_protocol("lambda");
+ phr.create_channel_semantic_protocol("result");
+ phr.create_channel_semantic_protocol("sigma");
+
+ pcm.set_direct_eval_fn(&phr_direct_eval);
+
+// pcm.create_and_register_type_object("PHR_Fn_Doc");
+ pcm.create_and_register_type_object("PHR_Fn_Doc*");
+
+ PHR_Env* penv = new PHR_Env(&pcm);
+ QString penv_typename = "PHR_Env*";
+ //??insert_envv(&penv_typename, penv);
+
+ PHR_Runtime_Scope prs(nullptr);
+
+ PHR_Symbol_Scope pss(&prs);
+
+ phr.init_table();
+
+ pcm.create_and_register_type_object("PHR_Env*");
+
+
+ init_test_functions(phr, pcm, *phr.table(), pss);
+
+ phr.hold_symbol_scope(&pss);
+
+ PHR_Command_Package pcp(&pcs, phr.type_system());
+ pcp.parse_from_file( DEFAULT_KPH_FOLDER "/dataset/raw/t1.kph" );
+
+ PHR_Channel_Group pcg(pcp);
+ PHR_Runner phrn;
+ phrn.run(pcg, &pss);
+
+ return 0;
+}
+
+int main1(int argc, char* argv[])
+{
+ KPH_Generator gen (DEFAULT_KPH_FOLDER "/gen/t1.txt");
+
+
+// Phaon_Runner phr;
+
+// Phaon_Namespace phn("TestNS");
+// Phaon_Class phc("Test_Class", &phn);
+
+// Kauvir_Code_Model& kcm = phr.get_kcm();
+// kcm.set_kph_generator(&gen);
+// kcm.set_direct_eval_fn(&kcm_direct_eval);
+// Phaon_Symbol_Scope pss;
+// init_test_functions(kcm, phr.get_table(), pss);
+// phr.get_phaon_scope_queue().push_front(&pss);
+
+// PHR_Command_Package pcp;
+// pcp.parse_from_file( DEFAULT_KPH_FOLDER "/dataset/raw/t1.kph" );
+
+ //PHR_Channel_Group pcg(kcm.channel_names());
+ //khp.init_channel_group(kcm, kcg);
+ //phr.run(kcg);
+
+ return 0;
+}
