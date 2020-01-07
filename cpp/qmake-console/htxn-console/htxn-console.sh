8f711cf84127c4023e386e61063c7eb2885e1dcf
scignscape-rz <osrworkshops@gmail.com>
2020-01-06 23:03:53 -0500

    update

--- cpp/qmake-console/htxn-console/htxn-console.sh
+++
@@ -1,2 +0,0 @@
-/home/nlevisrael/Qt5123/qt/5.12.3/gcc_64/bin/qmake "QMAKE_CONSOLE_ROOT=/home/nlevisrael/hypergr/ntxh/ar" /home/nlevisrael/hypergr/ntxh/ar/cpp/projects/qt/qtm/isobuild/htxn/htxn/htxn-console/htxn-console.pro
-
